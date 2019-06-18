5a20c6b5b57e774522b09abf3f2813906065e2da
UnchartedThoughts <50883809+UnchartedThoughts@users.noreply.github.com>
2019-06-17 22:40:45 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 var database = [];//GLOBAL VARIABLE FOR THE INCOMING DATA
-//FORMAT {brightness: 98, date: "[6,1,2019,"17:15"]", humidity: 79, temperature: 100, id: "1"}
+//FORMAT {brightness: 98, date: n(timestamp), humidity: 79, temperature: 100}
 
 //===========DATABASE==================================================/
 //Firebase configuration
@@ -20,33 +20,37 @@ const db = firebase.firestore();
 db.collection("chipLog").get().then(function(snapshot){//Then because async JS will load without data
   snapshot.docs.forEach(doc =>{
     let dataHold = doc.data();
-    dataHold.id = JSON.parse(doc.id);//adds ID to array parses it into number, the stupid firebase sets the id as string, i think it's to make it more hashable but I dont know, regardless, there is no option to change it
-    database.push(dataHold);//push object into database array
+    if(checkDataCorruption(dataHold)){//returns true for clear data
+      dataHold.date = (dataHold.date).toDate();//converts firestore's timestamp to date format (I know right?)
+      database.push(dataHold);//push object into database array
+    }
   });
   drawChartAndTable(database);//call function to draw tables and charts after data has been received
 }); 
 //===========DATABASE==================================================/
 
 //Calls chart and table's draw functions after data has been loaded
-function drawChartAndTable(database){//format reminder {brightness: 98, date: "[6,1,2019,"17:15"]", humidity: 79, temperature: 100, id: "1"}
+function drawChartAndTable(database){
 
-  //================LINE CHART==========================================/
+  //================LINE CHART TEMPERATURE==========================================/
   google.charts.load('current', {packages: ['corechart', 'line']});
-  google.charts.setOnLoadCallback(drawLineColors);
-  window.onresize = drawLineColors;//Runs the function everytime window is resized, responsive (could be done diferently so it doesn't have to redraw the whole chart but as you can see it took me one line)
+  google.charts.setOnLoadCallback(temperatureChart);
+  window.onresize = temperatureChart;//Runs the function everytime window is resized, responsive (could be done diferently so it doesn't have to redraw the whole chart but as you can see it took me one line)
 
-  function drawLineColors() {
+  function temperatureChart() {
     var data = new google.visualization.DataTable();
-    data.addColumn('number', 'Days Passed');
+    data.addColumn('date', 'Date');
     data.addColumn('number', 'Soil Temp');
     data.addColumn({type: 'string', role: 'tooltip'});
   
     let holdRows = [];
     for(let i=0;i<database.length;i++){//creates rows
       holdRows.push([
-        database[i].id,
+        database[i].date,
         database[i].temperature,
-        "Days Passed: "+database[i].id+" Days\nSoil Temperature: "+database[i].temperature+" F°"
+        "Date: "+database[i].date.toDateString()+" "
+        +database[i].date.toLocaleString('en-US', { hour: 'numeric', minute:'numeric' , hour12: true })
+        +"\nSoil Temperature: "+database[i].temperature+" F°"
       ]);
     };
 
@@ -54,21 +58,105 @@ function drawChartAndTable(database){//format reminder {brightness: 98, date: "[
 
     var options = {
       hAxis: {
-        title: 'Time(Days)'
+        title: 'Time(Date)'
       },
       vAxis: {
         title: 'Temperature(F°)'
       },
-      colors: ['#00dd55'],
+      colors: ['#dd5500'],
       series: {
               0: { lineWidth: 3 }
       },
     };
 
-    var chart = new google.visualization.LineChart(document.getElementById('temp_chart'));
+    var chart = new google.visualization.LineChart(document.getElementById('tempe_chart'));
     chart.draw(data, options);
   }
-  //================LINE CHART==========================================/
+  //================LINE CHART TEMPERATURE==========================================/
+
+  //================LINE CHART HUMIDITY==========================================/
+  google.charts.load('current', {packages: ['corechart', 'line']});
+  google.charts.setOnLoadCallback(humidityChart);
+  window.onresize = humidityChart;//Runs the function everytime window is resized, responsive (could be done diferently so it doesn't have to redraw the whole chart but as you can see it took me one line)
+
+  function humidityChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('date', 'Date');
+    data.addColumn('number', 'Soil Humid');
+    data.addColumn({type: 'string', role: 'tooltip'});
+  
+    let holdRows = [];
+    for(let i=0;i<database.length;i++){//creates rows
+      holdRows.push([
+        database[i].date,
+        database[i].humidity,
+        "Date: "+database[i].date.toDateString()+" "
+        +database[i].date.toLocaleString('en-US', { hour: 'numeric', minute:'numeric' , hour12: true })
+        +"\nSoil Humidity: "+database[i].humidity+"%"
+      ]);
+    };
+
+    data.addRows(holdRows);//sets rows
+
+    var options = {
+      hAxis: {
+        title: 'Time(Date)'
+      },
+      vAxis: {
+        title: 'Humidity(%)'
+      },
+      colors: ['#0055dd'],
+      series: {
+              0: { lineWidth: 3 }
+      },
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('humid_chart'));
+    chart.draw(data, options);
+  }
+  //================LINE CHART HUMIDITY==========================================/
+
+    //================LINE CHART BRIGHTNESS==========================================/
+    google.charts.load('current', {packages: ['corechart', 'line']});
+    google.charts.setOnLoadCallback(brightnessChart);
+    window.onresize = brightnessChart;//Runs the function everytime window is resized, responsive (could be done diferently so it doesn't have to redraw the whole chart but as you can see it took me one line)
+  
+    function brightnessChart() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'Date');
+      data.addColumn('number', 'Bright ness');
+      data.addColumn({type: 'string', role: 'tooltip'});
+    
+      let holdRows = [];
+      for(let i=0;i<database.length;i++){//creates rows
+        holdRows.push([
+          database[i].date,
+          database[i].brightness,
+          "Date: "+database[i].date.toDateString()+" "
+          +database[i].date.toLocaleString('en-US', { hour: 'numeric', minute:'numeric' , hour12: true })
+          +"\nBrightness: "+database[i].brightness+"%"
+        ]);
+      };
+  
+      data.addRows(holdRows);//sets rows
+  
+      var options = {
+        hAxis: {
+          title: 'Time(Date)'
+        },
+        vAxis: {
+          title: 'Brightness(%)'
+        },
+        colors: ['#aaee00'],
+        series: {
+                0: { lineWidth: 3 }
+        },
+      };
+  
+      var chart = new google.visualization.LineChart(document.getElementById('brigh_chart'));
+      chart.draw(data, options);
+    }
+    //================LINE CHART BRIGHTNESS==========================================/
 
   //================DATA TABLE==========================================/
   google.charts.load('current', {'packages':['table']});
@@ -76,8 +164,8 @@ function drawChartAndTable(database){//format reminder {brightness: 98, date: "[
 
   function drawTable() {
     var data = new google.visualization.DataTable();
-    data.addColumn('number', 'Id');
-    data.addColumn('string', 'Date');
+    data.addColumn('date', 'Date');
+    data.addColumn('string', 'Time');
     data.addColumn('number', 'Brightness');
     data.addColumn('number', 'Humidity');
     data.addColumn('number', 'Temperature');
@@ -85,8 +173,11 @@ function drawChartAndTable(database){//format reminder {brightness: 98, date: "[
     let holdRows = [];//this variable is local, don't worry about duplicates, don't use VAR though, that's global
     for(let i=0;i<database.length;i++){//creates rows
       holdRows.push([
-        database[i].id, database[i].date, database[i].brightness, 
-        database[i].humidity,database[i].temperature
+        database[i].date,
+        database[i].date.toLocaleString('en-US', { hour: 'numeric', minute:'numeric' , hour12: true }),
+        database[i].brightness, 
+        database[i].humidity,
+        database[i].temperature
       ]);
     };
     data.addRows(holdRows);
@@ -97,4 +188,17 @@ function drawChartAndTable(database){//format reminder {brightness: 98, date: "[
   }
   //================DATA TABLE==========================================/
   console.info(database);
+}
+
+function checkDataCorruption(obj){//checks the data for null/undefined values
+  let checkCounter = 0;
+  checkCounter += obj.brightness==null||undefined?0:1;
+  checkCounter += obj.humidity==null||undefined?0:1;
+  checkCounter += obj.temperature==null||undefined?0:1;
+  checkCounter += obj.date==null||undefined?0:1;
+  if (checkCounter < 4){
+    return false;
+  }else{
+    return true;//if date is clean returns true
+  }
 }
